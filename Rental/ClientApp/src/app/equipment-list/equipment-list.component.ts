f53975237a1d9d9013481dc8647671cad221efcb
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 22:47:34 +0200

    equipments list connected to cart

--- Rental/ClientApp/src/app/equipment-list/equipment-list.component.ts
+++ Rental/ClientApp/src/app/equipment-list/equipment-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { EquipmentService } from '../equipment.service';
+import { ShoppingCartService } from '../shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-equipment-list',
@@ -9,7 +11,11 @@ import { EquipmentService } from '../equipment.service';
 })
 export class EquipmentListComponent implements OnInit {
   equipments: any;
-  constructor(private equipmentService: EquipmentService) { }
+  constructor(
+    private equipmentService: EquipmentService,
+    private shoppingCartService: ShoppingCartService,
+    private _snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
     this.getEquipments();
@@ -18,5 +24,30 @@ export class EquipmentListComponent implements OnInit {
   getEquipments() {
     this.equipmentService.getEquipments()
     .subscribe(items => this.equipments = items, error => console.log(error));
+  }
+
+  addToCart(id: number, days: string) {
+    const parsedDays = parseInt(days);
+    if (!days || days == "") {
+      this.openSnackBar("\"Days\" is required", "Dismiss");
+      return
+    }
+    if (isNaN(parsedDays)) {
+      this.openSnackBar("Not a valid number", "Dismiss");
+      return
+    } 
+    if (parsedDays < 1) {
+      this.openSnackBar("\"Days\" can't be negative", "Dismiss");
+    }
+    else {
+      this.shoppingCartService.addItemToCart({equipmentId: id, days: parsedDays})
+      this.openSnackBar("Equipment added to cart", "Dismiss");
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 }
