f770782f2d69bdab68fe1887d2809a5d6a53c3b2
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 12:41:14 +0200

    renting context

---
+++ Rental/DbContexts/RentingContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rental.Entities;
+using Rental.Enums;
+
+namespace Rental.DbContexts
+{
+    public class RentingContext: DbContext
+    {
+        public RentingContext(DbContextOptions<RentingContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Equipment> Equipments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Equipment>().HasData(new Equipment { Name = "Caterpillar bulldozer", Type = EquipmentType.Heavy});
+            modelBuilder.Entity<Equipment>().HasData(new Equipment { Name = "KamAZ truck", Type = EquipmentType.Regular});
+            modelBuilder.Entity<Equipment>().HasData(new Equipment { Name = "Komatsu crane", Type = EquipmentType.Heavy});
+            modelBuilder.Entity<Equipment>().HasData(new Equipment { Name = "Volvo steamroller", Type = EquipmentType.Regular});
+            modelBuilder.Entity<Equipment>().HasData(new Equipment { Name = "Bosch jackhammer", Type = EquipmentType.Specialized});
+        }
+
+    }
+}
