b07125d9778416effad8be6906b4d6086734386d
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 15:38:18 +0200

    test card and empty list component

--- Rental/ClientApp/src/app/app-routing.module.ts
+++ Rental/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EquipmentListComponent } from './equipment-list/equipment-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EquipmentListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Rental/ClientApp/src/app/app.module.ts
+++ Rental/ClientApp/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EquipmentCardComponent } from './equipment-card/equipment-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { EquipmentListComponent } from './equipment-list/equipment-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipmentCardComponent,
+    EquipmentListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
