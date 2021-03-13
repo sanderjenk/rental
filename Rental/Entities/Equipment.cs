537268f08f7cfad7166f7e8849c9014325a167f0
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 12:13:27 +0200

    equipment entity

---
+++ Rental/Entities/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using Rental.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rental.Entities
+{
+    public class Equipment
+    {
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+    }
+}
---
+++ Rental/Enums/EquipmentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rental.Enums
+{
+    public enum EquipmentType
+    {
+        Heavy,
+        Regular,
+        Specialized
+    }
+}
