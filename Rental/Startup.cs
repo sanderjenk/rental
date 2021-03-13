ca3f02381e6a8f432631700434f644751d578c41
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 12:43:00 +0200

    add dbcontext to startup

--- Rental/Startup.cs
+++ Rental/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Rental.DbContexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Rental
 {
@@ -25,6 +27,9 @@ namespace Rental
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RentingContext>(opt => opt.UseInMemoryDatabase("Renting"));
+            services.AddScoped<RentingContext>();
+
             services.AddControllers();
         }
 
