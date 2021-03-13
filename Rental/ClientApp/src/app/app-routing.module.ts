e387081e4b586c2c8686689a20468c6a672d71e5
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 18:03:39 +0200

    updated routing and small changes

--- Rental/ClientApp/src/app/app-routing.module.ts
+++ Rental/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EquipmentListComponent } from './equipment-list/equipment-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
-  { path: '', component: EquipmentListComponent },
+  { path: 'equipments', component: EquipmentListComponent },
+  { path: 'cart', component: ShoppingCartComponent },
+  { path: '', pathMatch: "full", redirectTo: "equipments" },
 ];
 
 @NgModule({
