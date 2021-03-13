788b49f712612bee0856f81de228da8a5af8e779
Sander Jenk <sander.jenk@gmail.com>
2021-03-13 12:13:41 +0200

    initial equipments controller

---
+++ Rental/Controllers/EquipmentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rental.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Rental.Controllers
+{
+    [Route("api/equipments")]
+    [ApiController]
+    public class EquipmentsController : ControllerBase
+    {
+        // GET: api/<EquipmentsController>
+        [HttpGet]
+        public IEnumerable<Equipment> GetEquipments()
+        {
+            return new List<Equipment> {
+               new Equipment { Name = "Caterpillar bulldozer", Type = Enums.EquipmentType.Heavy} 
+            };
+        }
+    }
+}
